668be959a4b8d9ad11acd9aed963a52b434efd2b
Janno Kusman <janno.kusman@cyber.ee>
2024-12-09 16:32:16 +0200

    try to update documentation and scripts

--- shares-server/build-image.sh
+++ shares-server/build-image.sh
@@ -1,8 +1,17 @@
-# load env
-cd ..
-source load-env.sh
+#!/usr/bin/env bash
+# build Docker image locally
+#set -x
 
-cd shares-server
+SHARES_SERVER_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+DOCKER_REGISTRY=ghcr.io
+DOCKER_REPOSITORY=open-eid
+IMAGE_NAME=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
 
-# TODO: should spring-boot.build-image.publish=true only if env variable DOCKER_REGISTRY is set
-mvn spring-boot:build-image -Dspring-boot.build-image.publish=false -Dspring-boot.build-image.imageName=${DOCKER_REGISTRY}cdoc2-shares-server/shares-server:${SHARES_SERVER_VERSION} -Dspring-boot.build-image.createdDate=now
+LIQUIBASE_IMAGE_NAME=cdoc2-shares-server-liquibase
+
+mvn spring-boot:build-image \
+-Dmaven.test.skip=true \
+-Dspring-boot.build-image.publish=false \
+-Dspring-boot.build-image.imageName=${DOCKER_REGISTRY}/${DOCKER_REPOSITORY}/${IMAGE_NAME}:${SHARES_SERVER_VERSION} \
+-Dspring-boot.build-image.tags=${DOCKER_REGISTRY}/${DOCKER_REPOSITORY}/${IMAGE_NAME}:latest \
+-Dspring-boot.build-image.createdDate=now
