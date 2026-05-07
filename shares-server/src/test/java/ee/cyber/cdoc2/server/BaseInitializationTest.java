441692aebf17ac56319b94af0305bfb56951d133
Anti Ramst <anti.ramst@cyber.ee>
2026-05-07 09:26:06 +0300

    CDOC-150: quick workaround for Testcontainers problem

--- shares-server/src/test/java/ee/cyber/cdoc2/server/BaseInitializationTest.java
+++ shares-server/src/test/java/ee/cyber/cdoc2/server/BaseInitializationTest.java
@@ -1,9 +1,5 @@
 package ee.cyber.cdoc2.server;
 
-import ee.cyber.cdoc2.server.model.repository.KeyShareNonceRepository;
-import ee.cyber.cdoc2.server.model.repository.KeyShareRepository;
-import ee.cyber.cdoc2.server.model.repository.SessionNonceRepository;
-
 import jakarta.validation.ConstraintViolationException;
 
 import java.security.KeyStore;
@@ -20,17 +16,19 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import ee.cyber.cdoc2.server.model.repository.KeyShareNonceRepository;
+import ee.cyber.cdoc2.server.model.repository.KeyShareRepository;
+import ee.cyber.cdoc2.server.model.repository.SessionNonceRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 // Starts server on https
 // Starts PostgreSQL running on docker
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    properties = { "management.server.port=0" }
+    properties = {"management.server.port=0"}
 )
 @ContextConfiguration(initializers = BaseInitializationTest.Initializer.class)
 abstract class BaseInitializationTest {
@@ -45,12 +43,17 @@ abstract class BaseInitializationTest {
 
     @BeforeAll
     public static void startPostgresContainer() {
+        //TODO This is a workaround for
+        // https://github.com/testcontainers/testcontainers-java/issues/11212
+        // proper would probably upgrading to Testcontainers 2
+        System.setProperty("api.version", "1.44");
+
         postgresContainer = new PostgreSQLContainer<>("postgres:14.17") //Jammy 22.04 default version
             .withDatabaseName("integration-tests-db")
             .withUsername("sa")
             .withPassword("sa")
             .withReuse(true); //prevent pulling images repeatedly
-            //  Docker Hub imposes limits on anonymous users (100 pulls per 6 hours)
+        //  Docker Hub imposes limits on anonymous users (100 pulls per 6 hours)
         postgresContainer.start();
     }
 
