935f08b90b567a194b416a21db57f0fa7e7aa7f1
Hain Zuppur <hain.zuppur@cyber.ee>
2026-09-09 16:56:27 +0300

    CDOC-193: fix two existing tests

--- shares-server/src/test/java/ee/cyber/cdoc2/server/KeyShareApiTests.java
+++ shares-server/src/test/java/ee/cyber/cdoc2/server/KeyShareApiTests.java
@@ -21,6 +21,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
@@ -40,6 +41,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static ee.cyber.cdoc2.server.TestData.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 
@@ -75,6 +77,9 @@ class KeyShareApiTests extends KeyShareIntegrationTest {
 
     @Autowired
     private KeyShareApiService keyShareApiService;
+
+    @MockitoSpyBean
+    private ValidateAuthToken validateAuthToken;
 
     @Autowired
     private KeyShareExpiryConfigProperties keyShareExpiryConfig;
@@ -403,6 +408,10 @@ class KeyShareApiTests extends KeyShareIntegrationTest {
         );
 
         assertEquals(HttpStatus.UNAUTHORIZED.value(), ex.getCode());
+        // the 401 must come from session token validation, before auth token is even looked at,
+        // otherwise this test would also pass on an invalid/unparseable auth token, which is a
+        // different failure than the missing session nonce it is meant to cover
+        verifyNoInteractions(validateAuthToken);
     }
 
     @Test
@@ -434,6 +443,9 @@ class KeyShareApiTests extends KeyShareIntegrationTest {
         );
 
         assertEquals(HttpStatus.UNAUTHORIZED.value(), ex.getCode());
+        // same rationale as in getKeyShareShouldReturnUnauthorizedWhenSessionNonceMissing:
+        // pin the 401 to the expired session token, not to an invalid auth token
+        verifyNoInteractions(validateAuthToken);
     }
 
     private Cdoc2KeySharesApiClient createClient() throws Exception {
